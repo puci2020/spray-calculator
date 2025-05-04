610779f340d28ac79017374d0e5c082e611553c3
puci2020 <adam.pucicki97@gmail.com>
2025-05-04 19:10:59 +0200

    Change call google ads

--- components/AdSense.vue
+++ components/AdSense.vue
@@ -1,14 +1,21 @@
 <template>
       <div v-if="isClient">
-
         <ins
+      class="adsbygoogle"
+      style="display:block"
+      data-ad-client="ca-pub-1741709382102643"
+      data-ad-slot="6888503638"
+      data-ad-format="auto"
+      data-full-width-responsive="true"
+    ></ins>
+        <!-- <ins
           class="adsbygoogle"
           style="display:inline-block"
           :data-ad-client="adClient"
           :data-ad-slot="adSlot"
           data-ad-format="auto"
           data-full-width-responsive="true"
-        />
+        /> -->
 </div>
 </template>
   
@@ -19,6 +26,16 @@
     adClient: { type: String, required: true },
     adSlot: { type: String, required: true }
   })
+
+  useHead({
+  script: [
+    {
+      async: true,
+      src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1741709382102643',
+      crossorigin: 'anonymous',
+    },
+  ],
+});
   
   const isClient = ref(false)
 
